ac16576109c26f993002174dda005ae8ebcd38a5
Abhinay Reddy <abhinayreddy0112@gmail.com>
2025-05-14 22:50:26 +0530

    added wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import PortfolioTemplate1 from "./page/PortfolioTemplate1";
 import PortfolioTemplate2 from "./page/PortfolioTemplate2";
 import Home from "./page/Home";
 import Classic from "./pages/Classic";
-import "./App.css"
+import "./App.css";
 import PortfolioTemplate4 from "./page/PortfolioTemplate4";
 import PortfolioTemplate3 from "./page/PortfolioTemplate3";
-
+import AuthWrapper from "./AuthWrapper";
 
 function App() {
   return (
@@ -16,17 +16,39 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/cloud/userid" element={<h1>cloud userid page</h1>} />
           <Route
-            path="/template-1/userid"
-            element={<PortfolioTemplate1 />}
+            path="/modern/:userid"
+            element={
+              <AuthWrapper>
+                <PortfolioTemplate1 />
+              </AuthWrapper>
+            }
           />
           <Route
-            path="/template-2/userid"
-            element={<PortfolioTemplate2 />}
+            path="/creative/:userid"
+            element={
+              <AuthWrapper>
+                <PortfolioTemplate2 />
+              </AuthWrapper>
+            }
           />
           <Route path="/cloud/userid" element={<h1>Home</h1>} />
-          <Route path="/classic" element={<Classic/>}/>
-          <Route path="/template-3" element={<PortfolioTemplate3 />} />
-          <Route path="/template-4" element={<PortfolioTemplate4 />} />
+          <Route path="/classic" element={<Classic />} />
+          <Route
+            path="/professional/:userid"
+            element={
+              <AuthWrapper>
+                <PortfolioTemplate3 />
+              </AuthWrapper>
+            }
+          />
+          <Route
+            path="/tech/:userid"
+            element={
+              <AuthWrapper>
+                <PortfolioTemplate4 />
+              </AuthWrapper>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
